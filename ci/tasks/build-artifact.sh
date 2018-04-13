3da69b34b40c9e7ea901fda2c7f58ca2d71dd88c
Cameron Stewart <cstewart@pivotal.io>
2018-04-13 13:23:39 -0600

    sweet new feature

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -7,4 +7,4 @@ pushd git-repo/
   ./mvnw clean package
 popd
 
-cp git-repo/target/cfsummit-app-0.0.1-SNAPSHOT.jar   build-output/.
+cp git-repo/target/concourse-app-0.0.1-SNAPSHOT.jar   build-output/.
